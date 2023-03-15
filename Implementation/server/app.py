541fc110a2446b96d42e4c4869f78b55374f0d10
Sterfan <shiyifeng27@gmail.com>
2023-03-15 13:54:00 -0700

    dev_syf: finished doing month interface.

--- Implementation/server/app.py
+++ Implementation/server/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from controller import get_months
+import numpy as np
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +12,12 @@ CORS(app)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/basic/monthList")
+@cross_origin()
+def monthList():
+    month_list = get_months()
+    month_list2 = np.array(month_list, dtype=str).tolist()
+    return json.dumps({'monthList': month_list2})
 
 if __name__ == "__main__":
     app.run(port=3100, debug=True)
--- Implementation/server/controller.py
+++ Implementation/server/controller.py
@@ -1,3 +1,8 @@
 import pandas as pd
 import numpy as np
 
+from data_process.load_data import get_month_list
+
+
+def get_months():
+    return get_month_list()
