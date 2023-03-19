40140f81d38464b80d001c35ba8d1eb534726041
mshe <mshe@ucdavis.edu>
2023-03-18 18:43:07 -0700

    append supplementary file

---
+++ Implementation/dashboard/src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from 'vue'
+import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
+
+import { createPinia } from "pinia";
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { md3 } from 'vuetify/blueprints'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    blueprint: md3,
+})
+
+const app = createApp(App)
+app.use(vuetify)
+app.use(createPinia())
+app.use(ElementPlus)
+app.mount('#app')
